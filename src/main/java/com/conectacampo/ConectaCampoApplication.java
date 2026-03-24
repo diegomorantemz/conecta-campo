fcead94665f6c77bd7dae0fab174ff18cbb73413
Diego Morante <morantediego@outlook.com>
2026-03-24 14:57:43 -0500

    Agregado proyecto Spring Boot con dependencias iniciales

---
+++ src/main/java/com/conectacampo/ConectaCampoApplication.java
@@ -0,0 +1,13 @@
+package com.conectacampo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConectaCampoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConectaCampoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/conectacampo/ConectaCampoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.conectacampo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConectaCampoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
